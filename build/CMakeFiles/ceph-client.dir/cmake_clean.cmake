d4de877bbf3743cf3613287a0299ce0ef089ed93
lovethegirl <1797702937@qq.com>
2019-09-10 15:21:42 +0800

    测试cmakelist

---
+++ build/CMakeFiles/ceph-client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../ceph-client"
+  "../ceph-client.pdb"
+  "CMakeFiles/ceph-client.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ceph-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
