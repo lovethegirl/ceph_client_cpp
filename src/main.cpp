63077465eb48b381c905ee5572ddad837e53629d
ljw <1797702937@qq.com>
2019-09-16 16:44:02 +0800

    读写IO

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ int main(int argc, const char **argv)
         char cluster_name[] = "ceph";
         char user_name[] = "client.admin";
         uint64_t flags = 0;
+        const char * pool_name="hello_world_pool";
+        std::string hello("hello wrold");
+        std::string object_name("hw");
+        librados::IoCtx io_ctx;
 
         /* Initialize the cluster handle with the "ceph" cluster name and "client.admin" user */
         {
@@ -58,5 +62,70 @@ int main(int argc, const char **argv)
         }
         std::cout<<"this is a start"<<std::endl;
 
-        return 0;
+        ret = rados.pool_create(pool_name);
+        if(ret<0)
+        {
+           std::out<<"countn't create pool"<<std::endl;
+           return EXIT_FAILURE;                
+        }
+        std::cout<<"we jsut create a new pool named"<<pool_name<<std::endl;
+
+        ret = rados.ioctx_create(pool_name,io_ctx);
+        if(ret<0)
+        {
+                std::cout<<"ioctx_create fail"<<std::endl;
+                ret = EXIT_FAILURE;
+                goto out;
+        }
+        std::cout<<"we just create an ioctx for out pool"<<std::endl;
+
+        librados::bufferlist bl;
+        bl.append(hello);
+
+        ret = io_ctx.write_full(object_name,bl);
+        if(ret<0)
+        {
+                std::cout<<"count't write object "<<std::endl;
+                ret =  EXIT_FAILURE;
+                goto out;
+        }
+        std::cout<<"we jsut crote new object"<<object_name<<std::endl;
+
+        {
+    librados::bufferlist read_buf;
+    int read_len = 4194304; // this is way more than we need
+    // allocate the completion from librados
+    librados::AioCompletion *read_completion = librados::Rados::aio_create_completion();
+    // send off the request.
+    ret = io_ctx.aio_read(object_name, read_completion, &read_buf, read_len, 0);
+    if (ret < 0) {
+      std::cerr << "couldn't start read object! error " << ret << std::endl;
+      ret = EXIT_FAILURE;
+      goto out;
+    }
+    // wait for the request to complete, and check that it succeeded.
+    read_completion->wait_for_complete();
+    ret = read_completion->get_return_value();
+    if (ret < 0) {
+      std::cerr << "couldn't read object! error " << ret << std::endl;
+      ret = EXIT_FAILURE;
+      goto out;
+    }
+    std::cout << "we read our object " << object_name
+              << ", and got back " << ret << " bytes with contents\n";
+    std::string read_string;
+    read_buf.copy(0, ret, read_string);
+    std::cout << read_string << std::endl;
+  }
+  ret = EXIT_SUCCESS;
+
+        out:
+         int delete_ret=rados.pool_delete(pool_name);
+         if(delete_ret<0)
+         {
+                 std::cout<<"we faild to delete our test pool"<<std::endl;
+                 return = EXIT_FAILURE;
+         }
+         rados.shutdown();
+         return ret;
 }
