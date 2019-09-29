710a6a26b7821de95b5c0cb7567270d5358ce131
ljw <1797702937@qq.com>
2019-09-29 14:33:37 +0800

    更新Object 操作

--- include/cephclient.h
+++ include/cephclient.h
@@ -16,12 +16,19 @@ class CephClient
    ~CephClient();
    int Init(std::string pool_name,std::string cluster_name,std::string user_name,std::string path,std::string image_name);
    int Exit();
-//    int ImageCreate(uint64_t size,int order);
+   /******Imgae operation********/
+   int ImageCreate(uint64_t size,int order);
 //    int ImageRemove(std::string image_name);
 //    int ImageOpen();
 //    int Imagewrite();
 //    int Imageread();
 //    int ImageCreateSnap(std::string snap_name);
+   
+
+   /********Rados Opearation *******/
+   int ObjectWriet(std::string object_name,std::string buff);
+   int ObjectRead(std::string object_name,std::string & read_buff);
+   int ObjectRemove(std::string object_name);
  private:
    Name name;
    librados::Rados rados;
--- src/cephclient.cpp
+++ src/cephclient.cpp
@@ -9,9 +9,11 @@ CephClient::~CephClient()
 }
 int CephClient::Init(std::string pool_name,std::string cluster_name,std::string user_name,std::string path,std::string image_name)
 {
+    //初始化pool image 名字
     name.pool_name=pool_name;
     name.image_name=image_name;
     int ret=0;
+    //初始化集群
     ret = rados.init2(user_name.c_str(),cluster_name.c_str(),0);
     if(ret < 0)
     {
@@ -23,15 +25,16 @@ int CephClient::Init(std::string pool_name,std::string cluster_name,std::string 
         /* code */
          std::cout << "Created a cluster handle." << std::endl;
     }
+    //读取配置文件
     ret = rados.conf_read_file(path.c_str());
     if(ret<0) {
         std::cout << "Couldn't read the Ceph configuration file! error " << ret << std::endl;
         return EXIT_FAILURE;
     } else {
         std::cout << "Read the Ceph configuration file." << std::endl;
-    }
-    //{
-/*    ret = rados.conf_parse_argv(argc, argv);
+    } 
+    /* {
+   ret = rados.conf_parse_argv(argc, argv);
     if (ret < 0) {
         std::cerr << "Couldn't parse command line options! error " << ret << std::endl;
         return EXIT_FAILURE;
@@ -39,6 +42,7 @@ int CephClient::Init(std::string pool_name,std::string cluster_name,std::string 
         std::cout << "Parsed command line options." << std::endl;
         }
     }*/
+    //链接集群，初始化RadosClient
     ret = rados.connect();
     if(ret<0)
     {
@@ -50,7 +54,7 @@ int CephClient::Init(std::string pool_name,std::string cluster_name,std::string 
         std::cout << "Connected to the cluster." << std::endl;
     }
     
-
+    // 初始化iotcx
     ret = rados.ioctx_create(pool_name.c_str(),io_ctx);
     if(ret<0)
     {
@@ -67,3 +71,71 @@ int CephClient::Exit()
     rados.shutdown();
     return 0;
 }
+
+int CephClient::ImageCreate()
+{
+    return 0;
+}
+int CephClient::ObjectWriet(std::string object_name,std::string buff)
+{
+    librados::bufferlist bl;
+    bl.append(buff);
+    int ret = io_ctx.write_full(object_name,bl);
+    if(ret<0)
+    {
+                std::cout<<"count't write object "<<std::endl;
+                ret =  EXIT_FAILURE;
+                rados.shutdown();
+                return ret;
+    }
+    ret= EXIT_SUCCESS;
+    return ret;
+}
+int CephClient::ObjectRead(std::string object_name,std::string & read_buff)
+{
+    librados::bufferlist read_buf;
+    int read_len = 4194304; // this is way more than we need
+    // allocate the completion from librados
+    librados::AioCompletion *read_completion = librados::Rados::aio_create_completion();
+    // send off the request.
+    int ret = io_ctx.aio_read(object_name, read_completion, &read_buf, read_len, 0);
+    if (ret < 0) {
+      std::cerr << "couldn't start read object! error " << ret << std::endl;
+      ret = EXIT_FAILURE;
+      rados.shutdown();
+      return ret;
+    }
+    // wait for the request to complete, and check that it succeeded.
+    read_completion->wait_for_complete();
+    ret = read_completion->get_return_value();
+    if (ret < 0) {
+      std::cerr << "couldn't read object! error " << ret << std::endl;
+      ret = EXIT_FAILURE;
+      rados.shutdown();
+      return ret;
+    }
+    std::cout << "we read our object " << object_name
+              << ", and got back " << ret << " bytes with contents\n";
+    read_buf.copy(0, ret, read_buff);
+    std::cout << read_buff << std::endl;
+    return EXIT_SUCCESS;
+}
+
+int CephClient::ObjectRemove(std::string object_name)
+{
+     std::cout<<"start remove object "<<object_name<<std::endl;
+  int ret=io_ctx.remove(object_name);
+  {
+          if(ret<0)
+          {
+                  std::cout<<"remove object fail"<<std::endl;
+                  ret=EXIT_FAILURE;
+                  rados.shutdown();
+                  return ret;
+          }
+  }
+  std::cout<<"remove obejct sucessful"<<std::endl;
+  ret = EXIT_SUCCESS;
+  return ret;
+}
+
